95d48744611f45a5369f762868591ce0ef883b7c
xingyue425 <2006429019zk@163.com>
2020-06-11 17:46:53 +0800

    添加mvc配置文件

---
+++ testbaobao/src/main/java/com/zk/testbaobao/config/MyWebmvcConfiguration.java
@@ -0,0 +1,21 @@
+package com.zk.testbaobao.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+public class MyWebmvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter fjc = new FastJsonHttpMessageConverter();
+        FastJsonConfig fj = new FastJsonConfig();
+        fj.setSerializerFeatures(SerializerFeature.DisableCircularReferenceDetect);
+        fjc.setFastJsonConfig(fj);
+        converters.add(fjc);
+    }
+}
